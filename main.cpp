8333d92cf0e158a571a37f90f99dc85d65d525eb
NadnerbrendaN <brendanathome2008@gmail.com>
2025-10-05 17:18:06 -0400

    Started research and wrote a few lines

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-/*  Copyright (C) 2025 NadnerbrendaN <lunadock@protonmail.com> 
+/*  Copyright (C) 2025 Luna Dock <lunadock@protonmail.com> 
  *
  *  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  *  If you did not recieve a copy of the MPL-2.0 with this source code, please contact the author
  *  to report the issue, and visit https://www.mozilla.org/media/MPL/2.0/index.f75d2927d3c1.txt
  *  to obtain a copy of the license text.
 */
+
+#include <vector>
+
+std::vector<int> program_memory;
+std::vector<int> working_memory;
+
