7612a21fb0c05c0b6269945c6c9d60a830cb36dd
NadnerbrendaN <brendanathome2008@gmail.com>
2025-10-15 21:43:30 -0400

    Implement ADD and clean up stuff

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cppemu
-A 32-bit (?) custom instruction set emulator/interpreter written in C++
+A 32-bit ARM-compatible instruction set emulator/interpreter written in C++
+Read the LICENSE file for information on distribution/modification/copyright/etc
+The original plan was to implement my own, but I decided to go for an A32-compatible system first.
+
+## Important info:
+
+**It is not perfect**
+Don't expect it to have all the exact same details of execution, memory, etc any time soon.
+If that bothers you, contribute! Make it faster or more exact or whatever!
+
+**It's intended for simple educational use**
+Preferably as an ASM learning assistant -- that's how I'm using it!
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-/*  Copyright (C) 2025 Luna Dock <lunadock@protonmail.com> 
+/*
+ * Copyright (C) 2025 Luna Dock <lunadock@protonmail.com> 
  *
  *  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  *  If you did not recieve a copy of the MPL-2.0 with this source code, please contact the author
@@ -6,20 +7,68 @@
  *  to obtain a copy of the license text.
 */
 
-int registers[16];
-bool flags[4];
+#include <limits>
+#include <iostream>
 
-int add(bool s, short cond, int* rn, int* op2) {
-    flags[3] = ((*rn < 0) == (*op2 < 0) && (b > 0 && a > std::numeric_limits<int_type>::max() - b
-                || b < 0 && a < std::numeric_limits<int_type>::min() - b));
+unsigned int registers[16]; // 16 32-bit registers
+bool N = false; // Negative
+bool Z = false; // Zero
+bool C = false; // Carry
+bool V = false; // oVerflow
+
+bool check(short cond) { // check if the given condition should execute based on the status of our flags
+    switch (cond) {
+        case 0: // AL -- always execute
+            return true;
+        case 1: // EQ -- equal to 0
+            return Z;
+        case 2: // NE -- not equal to 0
+            return !Z;
+        case 3: // CS or HS -- C set / Higher or same
+            return C;
+        case 4: // CC or LO -- C clear / Lower
+            return !C;
+        case 5: // MI -- Minus
+            return N;
+        case 6: // PL -- Plus
+            return !N;
+        case 7: // VS -- V set
+            return V;
+        case 8: // VC -- V clear
+            return !V;
+        case 9: // HI -- Higher
+            return C && !Z;
+        case 10: // LS -- Lower or same
+            return !C || Z;
+        case 11: // GE -- signed >=
+            return N == V;
+        case 12: // LT -- signed <
+            return N != V;
+        case 13: // GT -- signed >
+            return N == V && !Z;
+        case 14: // LE -- signed <=
+            return N != V && Z;
+        default:
+            return true;
+    }
+}
+
+unsigned int add(bool s, unsigned int* rn, unsigned int* op2) { // ADD
     int res = 0;
-    if (!flags[3]) {
-        res = *rn + *op2;
-    } else {
-        
+    res = *rn + *op2; // add the values -- overflow is defined because it's unsigned
+    if (s) { // should we set the condition flags?
+        N = res < 0;
+        Z = res == 0;
+        C = (*op2 > 0 && *rn > std::numeric_limits<unsigned int>::max() - *op2);
+        V = C;
     }
-    if (s) {
-        flags[0] = res < 0;
-        flags[1] = res == 0;
-    }
+    return res;
+}
+
+int main() {
+    registers[0] = 255;
+    registers[1] = 1;
+    std::cout << add(false, &registers[0], &registers[1]);
+
+    return 0;
 }
